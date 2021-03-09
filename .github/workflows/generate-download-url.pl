2d436ea30e2c1a6e28d683527102b95e0d2f5b9d
JJ Merelo <jjmerelo@gmail.com>
2021-03-09 09:25:11 +0100

    Generates download URL

---
+++ .github/workflows/generate-download-url.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use JSON::PP;
+use LWP::Simple;
+
+use v5.14; # For say
+
+use constant ACTIONS_URL => 'https://api.github.com/repos/JJ/cv/actions/workflows/generate.yaml/runs';
+
+my $runs = decode_json get( ACTIONS_URL );
+
+my $artifacts = decode_json get(  $runs->{'workflow_runs'}[0]{'artifacts_url'} );
+
+say $artifacts->{'artifacts'}[0]{'archive_download_url'};
