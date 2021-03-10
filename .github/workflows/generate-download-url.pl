c35e58796ace643b6d6d7f029ad4ce67cd915623
JJ Merelo <jjmerelo@gmail.com>
2021-03-10 08:14:02 +0100

    Adds download URL file closes #2

--- .github/workflows/generate-download-url.pl
+++ .github/workflows/generate-download-url.pl
@@ -9,6 +9,7 @@ use LWP::Simple;
 use v5.14; # For say
 
 use constant ACTIONS_URL => 'https://api.github.com/repos/JJ/cv/actions/workflows/generate.yaml/runs';
+use constant URL_FILE => "download_cv_url.txt";
 
 my $runs = decode_json get( ACTIONS_URL );
 
@@ -17,9 +18,15 @@ my $check_suite_id =  $runs->{'workflow_runs'}[0]{'check_suite_id'};
 my $artifacts = decode_json get(  $runs->{'workflow_runs'}[0]{'artifacts_url'} );
 
 my $artifact_id = $artifacts->{'artifacts'}[0]{'id'};
+my $download_url = "https://github.com/JJ/cv/suites/$check_suite_id/artifacts/$artifact_id";
+
+open my $url_file, ">", URL_FILE;
+print $url_file $download_url;
+close $url_file;
+
 say <<EOC;
 # Download CV
 
 
-[Download zipped CV in PDF](https://github.com/JJ/cv/suites/$check_suite_id/artifacts/$artifact_id)
+[Download zipped CV in PDF]($download_url)
 EOC
