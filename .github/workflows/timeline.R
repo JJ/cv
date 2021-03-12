56d875f692d592b2ef21d5d527668da4840a5bb1
JJ Merelo <jjmerelo@gmail.com>
2021-03-12 18:51:27 +0100

    Reconstructs timeline

--- .github/workflows/timeline.R
+++ .github/workflows/timeline.R
@@ -1,6 +1,16 @@
 library("vistime")
+library("plotly")
 
 data <- read.csv("../../data/programming.csv")
 
 
-vistime(data, col.event="Item", col.start="Start.Date", col.end="End.Date", col.group="Group")
+g <- gg_vistime(data, col.event="Item", col.start="Start.Date", col.end="End.Date", col.group="Group") + theme(axis.text.x = element_text(angle=90, color='blue4',size=14) )+coord_flip()
+
+g.d <- ggplot_build(g)
+
+g.d$data[[4]]$angle <- 90
+
+rebuilt <- ggplot_gtable(g.d)
+
+plot(rebuilt)
+
