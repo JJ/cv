b66b772ce556f7bb71592c4d5ea557532c1d6ee6
JJ Merelo <jjmerelo@gmail.com>
2023-03-21 13:45:18 +0100

    Rendering to PDF #13
    Eliminating some non-needed items

--- bin/rmd-render.R
+++ bin/rmd-render.R
@@ -4,9 +4,5 @@ library(rmarkdown)
 library(knitr)
 args = commandArgs(trailingOnly=TRUE)
 
-print(args)
-
 render(args[1],
-       output_format = "md_document",
-       output_dir = "text",
-       output_file=paste0(args[1],".out"))
+       output_dir = "text")
